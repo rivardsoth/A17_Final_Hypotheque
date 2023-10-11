82b88e3bdde9b9a3c5123c6683890c324a0469f8
rivardsoth <117115621+rivardsoth@users.noreply.github.com>
2023-10-11 12:59:18 -0400

    premier commit

---
+++ app/src/main/java/com/example/a17_final_hypotheque/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.a17_final_hypotheque;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int option = item.getItemId();
+        if (option == R.id.mnCalculer) {
+
+        }else if (option == R.id.mnReour){
+
+        }else {
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
